84160fd5717e5cda5e8082b94ea585be0dcd0558
Martin Stancsics <martin.stancsics@gmail.com>
2024-05-14 12:13:20 +0200

    Fix conversion factor

--- settings.py
+++ settings.py
@@ -96,7 +96,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.15, participation_fee=10.00, doc=""
+    real_world_currency_per_point=0.12, participation_fee=10.00, doc=""
 )
 
 PARTICIPANT_FIELDS = ["final_payoff", "payoff_list", "task_score"]
